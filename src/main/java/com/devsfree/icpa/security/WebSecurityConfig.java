0f23b5c3ab966e3ea887b36162959f7053143a54
Samuel <sam6camargo@gmail.com>
2022-11-20 16:58:18 -0300

    removed bean

--- src/main/java/com/devsfree/icpa/security/WebSecurityConfig.java
+++ src/main/java/com/devsfree/icpa/security/WebSecurityConfig.java
@@ -59,18 +59,5 @@ public class WebSecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
-	@Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://127.0.0.1:5173");
-        config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
-        config.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
-    }
 
 }
